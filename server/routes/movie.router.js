94244bd13d20261bbf4f583096e18351841a26eb
Leila Skinner <beileiwang@gmail.com>
2020-04-25 10:52:00 -0500

    get movie details by id and get all movies with genres created on server and tested in postman

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -4,7 +4,11 @@ const pool = require("../modules/pool");
 
 // get all movies
 router.get("/", (req, res) => {
-  const queryText = `SELECT * FROM "movies" ORDER BY "title" ASC;`;
+  const queryText = `SELECT "movies".id, "movies".title, "movies".poster, 
+  "movies".description, array_agg(COALESCE("genres".name, 'N/A')) as "genres" 
+  FROM "genres" JOIN "movie_genre" ON "movie_genre".genre_id = "genres".id
+  RIGHT JOIN "movies" ON "movies".id = "movie_genre".movie_id 
+  GROUP BY "movies".id ORDER BY "movies".title;`;
   pool
     .query(queryText)
     .then((responseFromDb) => {
@@ -15,8 +19,45 @@ router.get("/", (req, res) => {
       res.sendStatus(500);
     });
 });
-// put/update movies
 
 // get movie details
+router.get("/details/:id", (req, res) => {
+  const queryText = `SELECT "movies".id, "movies".title, "movies".poster, 
+  "movies".description, array_agg(COALESCE("genres".name, 'N/A')) as "genres" 
+  FROM "genres" JOIN "movie_genre" ON "movie_genre".genre_id = "genres".id
+  RIGHT JOIN "movies" ON "movies".id = "movie_genre".movie_id 
+  WHERE "movies".id = $1 GROUP BY "movies".id;`;
+  pool
+    .query(queryText, [req.params.id])
+    .then((responseFromDb) => {
+      res.send(responseFromDb.rows);
+    })
+    .catch((error) => {
+      console.log("GET DETAILS ERROR: ", error);
+      res.sendStatus(500);
+    });
+});
+
+// put/update movies
+router.put("/edit", (req, res) => {
+  const movieData = req.body;
+  const queryText = `UPDATE "movies" 
+SET "title" = $1, "poster" = $2, "description" = $3
+WHERE "id" = $4;`;
+  pool
+    .query(queryText, [
+      movieData.title,
+      movieData.poster,
+      movieData.description,
+      movieData.id,
+    ])
+    .then((responseFromDb) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log("PUT ERROR: ", error);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
-const router = require("./routes/movie.router");
+const movieRouter = require("./routes/movie.router");
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static("build"));
 
 /** ---------- ROUTES ---------- **/
-app.use("/movies", router);
+app.use("/movies", movieRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
